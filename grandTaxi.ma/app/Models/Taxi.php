fa899ac610aedcb36217e41cc55b483e3c43d902
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-12 15:17:20 +0100

    J'ai ajouter le model de ville

--- grandTaxi.ma/app/Models/Taxi.php
+++ grandTaxi.ma/app/Models/Taxi.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Taxi extends Model
 {
-    //
+    protected $fillable = ['matricule', 'capacite', 'status', 'driver_id'];
+
+    public function chauffeur()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
+
