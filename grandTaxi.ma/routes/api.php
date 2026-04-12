a236a092bdcb8992181375ca81004befa4d84d46
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-12 16:46:13 +0100

    J'ai ajouter les routes de trajet

--- grandTaxi.ma/routes/api.php
+++ grandTaxi.ma/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\VilleController;
+use App\Http\Controllers\TrajetController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -21,4 +22,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/admin/villes', [VilleController::class, 'store']);
     Route::put('/admin/villes/{ville}', [VilleController::class, 'update']);
     Route::delete('/admin/villes/{ville}', [VilleController::class, 'destroy']);
+});
+
+// routes trajet
+Route::get('/trajets', [TrajetController::class, 'index']);
+Route::get('/trajets/{trajet}', [TrajetController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/admin/trajets', [TrajetController::class, 'store']);
+    Route::put('/admin/trajets/{trajet}', [TrajetController::class, 'update']);
+    Route::delete('/admin/trajets/{trajet}', [TrajetController::class, 'destroy']);
+    Route::patch('/admin/trajets/{trajet}/cloturer', [TrajetController::class, 'cloturer']);
 });
