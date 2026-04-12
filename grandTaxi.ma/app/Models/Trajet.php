b7f01a73f29fe2a54941197ef634e2a2f11a3cf4
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-12 16:48:04 +0100

    J'ai ajouter le model de trajet et les relation

--- grandTaxi.ma/app/Models/Trajet.php
+++ grandTaxi.ma/app/Models/Trajet.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trajet extends Model
 {
-    //
+    protected $fillable = [
+        'ville_depart_id',
+        'ville_arrivee_id',
+        'prix',
+        'statut',
+    ];
+
+    // ville depart
+    public function villeDepart()
+    {
+        return $this->belongsTo(Ville::class, 'ville_depart_id');
+    }
+
+    // ville arrivee
+    public function villeArrivee()
+    {
+        return $this->belongsTo(Ville::class, 'ville_arrivee_id');
+    }
+
+    // les taxi de ce trajet
+    // public function taxi()
+    // {
+    //     return $this->belongsTo(Taxi::class, 'taxi_id');
+    // }
 }
