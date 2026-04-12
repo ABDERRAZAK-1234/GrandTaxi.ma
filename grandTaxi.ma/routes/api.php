c624f4a5a52d9f9c0f79100f6ee7b43db5968c8a
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-12 15:18:05 +0100

    J'aji ajouter les routes pour le crud de ville

--- grandTaxi.ma/routes/api.php
+++ grandTaxi.ma/routes/api.php
@@ -2,11 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\VilleController;
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/user', function (Request $request) {
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+// routes of ville
+Route::get('/villes', [VilleController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/admin/villes', [VilleController::class, 'store']);
+    Route::put('/admin/villes/{ville}', [VilleController::class, 'update']);
+    Route::delete('/admin/villes/{ville}', [VilleController::class, 'destroy']);
+});
