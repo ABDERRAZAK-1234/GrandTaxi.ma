6d7a5c471b8727e931566b72100f4846895c2871
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-19 15:41:06 +0100

    Ajouter les routes api de taxi & de reservations

--- grandTaxi.ma/routes/api.php
+++ grandTaxi.ma/routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\VilleController;
 use App\Http\Controllers\TrajetController;
+use App\Http\Controllers\TaxiController;
+use App\Http\Controllers\ReservationController;
+
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,6 +18,7 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 // routes of ville
 Route::get('/villes', [VilleController::class, 'index']);
@@ -27,10 +32,32 @@ Route::middleware('auth:sanctum')->group(function () {
 // routes trajet
 Route::get('/trajets', [TrajetController::class, 'index']);
 Route::get('/trajets/{trajet}', [TrajetController::class, 'show']);
+Route::get('/trajets/{trajet}/taxis', [TaxiController::class, 'parTrajet']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/admin/trajets', [TrajetController::class, 'store']);
     Route::put('/admin/trajets/{trajet}', [TrajetController::class, 'update']);
     Route::delete('/admin/trajets/{trajet}', [TrajetController::class, 'destroy']);
     Route::patch('/admin/trajets/{trajet}/cloturer', [TrajetController::class, 'cloturer']);
+});
+
+// routes taxi
+Route::get('/taxis', [TaxiController::class, 'index']);
+Route::get('/taxis/{taxi}', [TaxiController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/admin/taxis', [TaxiController::class, 'store']);
+    Route::put('/admin/taxis/{taxi}', [TaxiController::class, 'update']);
+    Route::delete('/admin/taxis/{taxi}', [TaxiController::class, 'destroy']);
+});
+
+// routes reservations
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
+    Route::post('/reserver', [ReservationController::class, 'reserver']);
+    Route::post('/reserver/confirmer', [ReservationController::class, 'confirmerReservation']);
+
+    // Admin
+    Route::get('/admin/reservations', [ReservationController::class, 'adminIndex']);
 });
