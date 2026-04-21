cf5f5b571dd1123167aed05c85ff633a7bd7968f
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-21 11:35:28 +0100

    Implémentation du broadcasting en temps réel pour les réservations par trajet

--- grandTaxi.ma/app/Events/ReservationCreated.php
+++ grandTaxi.ma/app/Events/ReservationCreated.php
@@ -5,16 +5,20 @@ namespace App\Events;
 use App\Models\Reservation;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ReservationCreated implements ShouldBroadcast
+class ReservationCreated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public function __construct(public Reservation $reservation)
-    {}
+    public Reservation $reservation;
+
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
 
     public function broadcastOn(): array
     {
@@ -32,9 +36,10 @@ class ReservationCreated implements ShouldBroadcast
     {
         return [
             'reservation_id' => $this->reservation->id,
-            'taxi_id'        => $this->reservation->taxi_id,
-            'trajet_id'      => $this->reservation->trajet_id,
-            'nombre_place'   => $this->reservation->nombre_place,
+            'taxi_id' => $this->reservation->taxi_id,
+            'trajet_id' => $this->reservation->trajet_id,
+            'sieges' => $this->reservation->sieges,
+            'nombre_place' => $this->reservation->nombre_place,
             'places_restantes' => $this->getPlacesRestantes(),
         ];
     }
