f36b376babf7520f3d036fe3e49b442b64d61668
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-24 09:14:44 +0100

    ajouter un champ image et une URL par défaut

--- grandTaxi.ma/app/Models/Taxi.php
+++ grandTaxi.ma/app/Models/Taxi.php
@@ -13,6 +13,7 @@ class Taxi extends Model
         'statuts',
         'driver_id',
         'trajet_id',
+        'image',
     ];
 
     public function driver()
@@ -29,6 +30,22 @@ class Taxi extends Model
     {
         return $this->hasMany(Reservation::class);
     }
+
+
+    public function getImageUrlAttribute(): string
+    {
+        return $this->image
+            ? asset('storage/' . $this->image)
+            : asset('images/taxi_default.png');
+    }
+
+    protected $appends = ['image_url'];
+
+    public function driverProfile()
+    {
+        return $this->hasOne(DriverProfile::class);
+    }
+
 }
 
 
