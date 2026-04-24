efddcad8831f9ee6ad7d7bcc47d988b15f6e3934
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-24 09:24:51 +0100

    configurer Laravel Echo avec Pusher pour Reverb

--- grandTaxi.ma/resources/js/bootstrap.js
+++ grandTaxi.ma/resources/js/bootstrap.js
@@ -1,4 +1,26 @@
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.Pusher = Pusher;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import "./echo";
+
+window.Echo = new Echo({
+    broadcaster: "reverb",
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: false,
+    enabledTransports: ["ws", "wss"],
+});
